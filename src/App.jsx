5a3c660c8d5af669be8af86a697a6bb1b555b165
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-10-22 15:44:15 +0600

    project setup and nav add

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navber from './Components/Navber/Navber'
+
+function App() {
+
+
+  return (
+    <>
+      <header className=''>
+        <Navber />
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navber/Navber.jsx
@@ -0,0 +1,13 @@
+import profileImg from "/images/profile.png";
+function Navber() {
+  return (
+    <nav className="wrap">
+      <div className="flex justify-between items-center  border-b ">
+        <h4 className="heading">Knowladge Cafe</h4>
+        <img className="my-2" src={profileImg} alt="" />
+      </div>
+    </nav>
+  );
+}
+
+export default Navber;
